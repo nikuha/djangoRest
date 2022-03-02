cc702b6aff4a68400e1b774579c6a86d4056dfcf
nikuha <yoginika@yandex.ru>
2022-03-02 21:39:31 +0300

    lesson03 -permission read only -project and todo models -serializers

--- service/service/settings.py
+++ service/service/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'users'
+    'users',
+    'todo'
 ]
 
 MIDDLEWARE = [
@@ -125,12 +126,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 
-if not DEBUG:
-    REST_FRAMEWORK = {
-        'DEFAULT_RENDERER_CLASSES': (
-            'rest_framework.renderers.JSONRenderer',
-        )
-    }
+REST_FRAMEWORK = {
+    # 'DEFAULT_RENDERER_CLASSES': (
+    #     'rest_framework.renderers.JSONRenderer',
+    # ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    )
+}
 
 CORS_ALLOWED_ORIGINS = [
    "http://localhost:3000",
--- service/users/management/commands/fill_db.py
+++ service/users/management/commands/fill_db.py
@@ -1,12 +1,16 @@
+import random
+from lorem_text import lorem
 from django.core.management.base import BaseCommand
+
+from todo.models import Project, Todo
 from users.models import User
 from random import choice
 from string import ascii_lowercase
 
 
-def random_string(length):
-    letters = ascii_lowercase
-    return ''.join(choice(letters) for i in range(length))
+# def random_string(length):
+#     letters = ascii_lowercase
+#     return ''.join(choice(letters) for i in range(length))
 
 
 class Command(BaseCommand):
@@ -15,7 +19,24 @@ class Command(BaseCommand):
         if not User.objects.filter(username='django').exists():
             User.objects.create_superuser(username='django', email='django@rest.local', password='geekbrains')
 
+        users = []
         for i in range(1, 10):
-            email = f'{random_string(10)}@{random_string(5)}.{random_string(2)}'
-            User.objects.create(username=random_string(10), email=email, first_name=random_string(8),
-                                last_name=random_string(10))
+            username = lorem.words(1)
+            email = f'{username}@{lorem.words(1)}.com'
+            user = User.objects.create(username=lorem.words(1), email=email, first_name=lorem.words(1).capitalize(),
+                                       last_name=lorem.words(1).capitalize())
+            users.append(user)
+
+        projects = []
+        for i in range(1, 4):
+            name = f'Название проекта № {i}'
+            repository = f'https://github.com/{lorem.words(1)}/{lorem.words(1)}'
+            project = Project.objects.create(name=name, repository=repository)
+            projects.append(project)
+
+            for project_user in random.sample(users, random.randint(1, 5)):
+                project.users.add(project_user)
+
+                for j in range(random.randint(0, 5)):
+                    Todo.objects.create(user=project_user, project=project, text=lorem.sentence(),
+                                        done=random.randint(0, 1))
